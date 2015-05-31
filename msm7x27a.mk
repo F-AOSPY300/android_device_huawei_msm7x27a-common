829aacb423f277c3171b06d67e97e9b2848c9449
fonz93 <fonz993@hotmail.it>
2015-05-31 22:29:24 +0200

    remove some custom ART properties
    from Google: The options that control initial and maximum heap size for dex2oat should not be reduced since they could limit what applications can be compiled.

--- msm7x27a.mk
+++ msm7x27a.mk
@@ -209,11 +209,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
    dalvik.vm.dex2oat-flags=--no-watch-dog \
    dalvik.vm.image-dex2oat-filter=speed
 
-# ART properties
-ADDITIONAL_DEFAULT_PROPERTIES += \
-   dalvik.vm.dex2oat-Xms=8m \
-   dalvik.vm.dex2oat-Xmx=96m \
-   dalvik.vm.image-dex2oat-Xms=48m \
-   dalvik.vm.image-dex2oat-Xmx=48m
-
 $(call inherit-product, vendor/huawei/msm7x27a-common/msm7x27a-common-vendor.mk)
